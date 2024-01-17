bf50bc80113f74e791b5dd14d2c26ee8ac9243a8
Joe Andrews <joeandrews92@gmail.com>
2024-01-16 16:11:29 -0800

    Add score and highscore logic to handleClick function. Render score and high score.

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,28 @@ import Fetch from './Fetch.jsx'
 import Card from './Card.jsx'
 function App() {
   const [chars, setChars] = useState([]);
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [selected, setSelected] = useState([]);
 
   return (
     <>
       <h1>Bobs Burgers</h1>
+      <h2>Score: {score}</h2>
+      <h3>High Score: {highScore}</h3>
+      <p>{selected.map(id => <span key={id}>{id}, </span>)}</p>
       <Fetch setChars={setChars}/>
       <div className="container">
-        <Card chars={chars} setChars={setChars}/>
+        <Card 
+        chars={chars} 
+        setChars={setChars} 
+        score={score} 
+        setScore={setScore}
+        selected={selected}
+        setSelected={setSelected}
+        highScore={highScore}
+        setHighScore={setHighScore}
+        />
       </div>
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,32 @@
-export default function Card({chars, setChars}){
+import { useState } from 'react'
+
+export default function Card({chars, setChars, score, setScore, selected, setSelected, highScore, setHighScore}){
     
+
     const shuffle = (array) => { 
         return array.sort(() => Math.random() - 0.5); 
     }; 
 
-    function handleClick(){
+    function handleClick(id){
         setChars(shuffle([...chars]))
+
+        if (selected.find((card) => card == id)){
+            console.log("game over")
+            if (score > highScore){
+                setHighScore(score)
+            }
+            setScore(0)
+            setSelected([])
+        } else {
+            setScore(score + 1)
+            setSelected([...selected, id])
+        }
+ 
     }
 
     return (
         chars.map(char => 
-            <div className="card" key={char.id} onClick={handleClick}>
+            <div className="card" key={char.id} onClick={ () => handleClick(char.id)}>
                 <img src={char.image} alt={char.title} width={100}></img>
                 <p>{char.name}</p>
             </div>
