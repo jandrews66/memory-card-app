4ff6d58e10038621fb5126c64c509406115e1460
Joe Andrews <joeandrews92@gmail.com>
2024-01-16 18:11:49 -0800

    Add styling to cards and create an API call for burger of the day data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react'
 
 import './App.css'
-import Fetch from './Fetch.jsx'
+import {Fetch} from './Fetch.jsx'
+import {FetchBurg} from './Fetch.jsx'
 import Card from './Card.jsx'
+
 function App() {
   const [chars, setChars] = useState([]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [selected, setSelected] = useState([]);
+  const [burg, setBurg] = useState([]);
 
   return (
     <>
-      <h1>Bobs Burgers</h1>
+      <h1>Bobs Burgers Memory Game</h1>
       <h2>Score: {score}</h2>
       <h3>High Score: {highScore}</h3>
-      <p>{selected.map(id => <span key={id}>{id}, </span>)}</p>
-      <Fetch setChars={setChars}/>
+      <p>{burg.name} {burg.price}</p>
+      <Fetch 
+      setChars={setChars}
+      />
+      <FetchBurg 
+        setBurg={setBurg}
+      />
       <div className="container">
         <Card 
         chars={chars} 
--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 
-const Fetch = ({setChars}) => {
+export const Fetch = ({setChars}) => {
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://bobsburgers-api.herokuapp.com/characters/51,167,280,275,467,457,227,317,155,18,410,391');
+        const response = await fetch('https://bobsburgers-api.herokuapp.com/characters/51,167,280,275,467,457,227,212,441,18,184,391');
+        //const response = await fetch('https://bobsburgers-api.herokuapp.com/characters/');
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
@@ -18,8 +19,26 @@ const Fetch = ({setChars}) => {
 
     fetchData();
   }, []);
-
-
 };
+export const FetchBurg = ({setBurg}) => {
+    useEffect(() => {
+        const fetchData = async () => {
+            const ranNum = Math.floor(Math.random() * 9) + 1
+    
+          try {
+            const response = await fetch(`https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${ranNum}`);
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            console.log(data)
+            setBurg(data)
+          } catch (error) {
+            console.error('Error fetching data:', error.message);
+          }
+        };
+    
+        fetchData();
+      }, []);    
 
-export default Fetch;
+}
